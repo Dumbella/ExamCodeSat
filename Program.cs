130cbc5c4212fe7d1c9ba69b2c65ee05de6dc391
Dumbella <78536856+Dumbella@users.noreply.github.com>
2021-04-24 14:22:12 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace examSat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mode = Console.ReadLine();
+            float filmBackWidth = float.Parse(Console.ReadLine());
+            if (filmBackWidth > 0)
+            {
+                do
+                    {
+                        Console.WriteLine("invalid filmBackWidth please input again.");
+                        filmBackWidth = float.Parse(Console.ReadLine());
+
+                     } while (filmBackWidth !> 0);
+            }
+            else
+            {
+                float fLength, fov;
+
+                if (mode == "fLength")
+                {
+                    do
+                    {
+                        Console.WriteLine("invalid fLength please input again.");
+                        fLength = float.Parse(Console.ReadLine());
+
+                    } while (fLength !> 0);
+                    if (fLength>0)
+                    {
+                        fov = 2 * Math.Atan(filmBackWidth / 2, fLength);
+                        
+                    }
+                    
+                }
+                else if(mode == "fov")
+                {
+                    do
+                    {
+                        Console.WriteLine("invalid fov. please input again.");
+                        fov = float.Parse(Console.ReadLine());
+
+                    } while (fov !> 0.1 && fov !< 6.28 );
+
+                    if (fov > 0.1 && fov < 6.28)
+                    {
+                        fLength = filmBackWidth /( 2* Math.Atan(fov/2));
+
+                        Console.WriteLine("fLength = {0}, fov = {1}", fLength, fov);}
+                    }
+                }
+                
+            
+
+            
+        }
+    }
+}
