ce054c81f37b4f813fa1d7bbec08c105f702c553
Dumbella <78536856+Dumbella@users.noreply.github.com>
2021-04-24 14:58:32 +0700

    Add files via upload
    ข้อ3

---
+++ 3.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace examSat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mode = Console.ReadLine();
+            double filmBackWidth = double.Parse(Console.ReadLine());
+            if (filmBackWidth > 0)
+            {
+                do
+                    {
+                        Console.WriteLine("invalid filmBackWidth please input again.");
+                        filmBackWidth = double.Parse(Console.ReadLine());
+
+                     } while (filmBackWidth !> 0);
+            }
+            else
+            {
+                double fLength, fov;
+
+                if (mode == "fLength")
+                {
+                    do
+                    {
+                        Console.WriteLine("invalid fLength please input again.");
+                        fLength = double.Parse(Console.ReadLine());
+
+                    } while (fLength !> 0);
+                    if (fLength>0)
+                    {
+                        fov = 2 * Math.Atan(filmBackWidth / (2 * fLength));
+                        Console.WriteLine("fLength = {0}, fov = {1}", fLength, fov);
+                    }
+                    
+                }
+                else if(mode == "fov")
+                {
+                    do
+                    {
+                        Console.WriteLine("invalid fov. please input again.");
+                        fov = double.Parse(Console.ReadLine());
+
+                    } while (fov !> 0.1 && fov !< 6.28 );
+
+                    if (fov > 0.1 && fov < 6.28)
+                    {
+                        fLength = filmBackWidth / (2 * Math.Atan(fov / 2));
+
+                        Console.WriteLine("fLength = {0}, fov = {1}", fLength, fov);
+                    }
+                    }
+                }
+                
+            
+
+            
+        }
+    }
+}
